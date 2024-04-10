4c72875deab4cc30f20c3bbc14ac78e72e3714ce
Slickpic1 <slickpic174@gmail.com>
2024-04-10 12:58:31 -0600

    Finished assignment.

--- W05Prove/receipt.py
+++ W05Prove/receipt.py
@@ -1,5 +1,6 @@
 #Import Libraries
 import csv
+from datetime import datetime
 
 def read_dictionary(filename,key_column_index):
     """Read the contents of a CSV file into a compound
@@ -31,6 +32,87 @@ def read_dictionary(filename,key_column_index):
 
     return dictionary
 
+def test_setup():
+    PROCUDT_ID_INDEX = 0
+    try: 
+        #Check to see if the file that we are looking for exists
+        open("products.csv")
+        open("request.csv")
+
+        #Check to see if our request has an invadil file
+        products_dict = read_dictionary("products.csv",0)
+        with open("request.csv","rt") as csv_file:
+            reader = csv.reader(csv_file)
+            next(reader)
+            for row in reader:
+                #Get product id
+                product_id = row[PROCUDT_ID_INDEX]
+                products_dict[product_id]
+                
+    except FileNotFoundError as file_not_found_err:
+        print("Error: missing file")
+        print(file_not_found_err)
+        return False
+    
+    except KeyError as key_error:
+        print("Error: unknown product ID in the request.csv file")
+        print(key_error)
+        return False
+    
+    else:
+        return True
+    
+def CalcSalesTax(subtotal):
+    return subtotal*0.06
+
+def calc_discount(subtotal):
+    
+    return subtotal*.10
+
+def display_receipt(item_list,subtotal,total_quantity):
+    store_name = "Inkom Emporium"
+
+    #Find out the day of the week
+    dt = datetime.now()
+    day = dt.weekday()
+
+    #Print out name of store and two newlines
+    print(store_name,"\n")
+
+    for item in item_list:
+        print(item)
+
+    print()  #another newline
+
+    #Check to see if a discount can be applied
+    if (day == 1 or day == 2):
+        discount = calc_discount(subtotal)
+        subtotal -= discount
+        print(f"Discount amount: {discount:.2f}")
+
+    #Calculate our salestax
+    salestax = CalcSalesTax(subtotal)
+
+    #Add up total
+    total = subtotal + salestax
+
+    #Print cost section
+    print(f"Number of Items: {total_quantity}")
+    print(f"Subtotal: {subtotal:.2f}")
+    print(f"Sales Tax: {salestax:.2f}")
+    print(f"Total: {total:.2f}")
+
+    #Print thank you message
+    print(f"Thank you for shopping at the {store_name}.")
+    print(f"{dt:%A %I:%M %p}")
+
+    #Print survey
+    print("\n Want to win a genuine nuclear warhead?")
+    print("Take our online survey for a chance to win at:")
+    print("  www.psyop.gov/defnotentrpmnt")
+
+    return
+
 def main():
     #Define Indeces
     PROCUDT_ID_INDEX = 0
@@ -38,15 +120,17 @@ def main():
     QUANTITY_INDEX = 1
     PRICE_INDEX = 2
 
+    #Define variables
+    total_quantity = 0
+    subtotal = 0
+    item_list = []
+
+    #Check for errors
+    if (test_setup() == False):
+        return
+
     #Call read dictionary for products.csv
     products_dict = read_dictionary("products.csv",0)
-
-    #Print out all products
-    print("All Products")
-    print(products_dict)
-
-    #Print out first line of requested items
-    print("Requested items:")
 
     #Opens and reads the request.csv file
     with open("request.csv","rt") as csv_file:
@@ -64,11 +148,20 @@ def main():
 
             #Get product name, price, and quantity
             product_name = products_dict[product_id][PRODUCT_NAME_INDEX]
-            product_price = products_dict[product_id][PRICE_INDEX]
-            quantity = row[QUANTITY_INDEX]
+            product_price = float(products_dict[product_id][PRICE_INDEX])
+            quantity = int(row[QUANTITY_INDEX])
 
-            #Print our requested items
-            print(f'{product_name}: {quantity} @ {product_price}')
+            #Add quanitity to our total_quantity
+            total_quantity += quantity
+
+            #Calculate our subtotal and add to total_prive
+            subtotal += quantity*product_price
+
+            #Add item to our list
+            item_list.append(f'{product_name}: {quantity} @ {product_price}')
+
+    #Print our receipt
+    display_receipt(item_list,subtotal,total_quantity)
 
     return
 
